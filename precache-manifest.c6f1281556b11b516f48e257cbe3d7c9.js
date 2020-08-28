6028edb0d55e6f96c04a0df14d5fc6ddb6552378
gaozw <gaozw@venpoo.com>
2020-08-28 14:54:25 +0800

    Updates

---
+++ precache-manifest.c6f1281556b11b516f48e257cbe3d7c9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fe13dc0a344fc223f674b4a208656d3",
+    "url": "/hanxin/index.html"
+  },
+  {
+    "revision": "a53d3a097b53c6cf017c",
+    "url": "/hanxin/static/css/2.af458045.chunk.css"
+  },
+  {
+    "revision": "ae0d54a2b128124e8e66",
+    "url": "/hanxin/static/css/main.9a6bd689.chunk.css"
+  },
+  {
+    "revision": "a53d3a097b53c6cf017c",
+    "url": "/hanxin/static/js/2.d55158d4.chunk.js"
+  },
+  {
+    "revision": "3ae122318a14b6479c63e0d6a0223996",
+    "url": "/hanxin/static/js/2.d55158d4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ae0d54a2b128124e8e66",
+    "url": "/hanxin/static/js/main.848e7f7e.chunk.js"
+  },
+  {
+    "revision": "2e4a34af08045c780ad7",
+    "url": "/hanxin/static/js/runtime-main.7e964ef0.js"
+  },
+  {
+    "revision": "ca2f6f6a27ed94c33887580ba432177c",
+    "url": "/hanxin/static/media/2.ca2f6f6a.jpg"
+  },
+  {
+    "revision": "a568162c2e16b799f5d56634d9e23e46",
+    "url": "/hanxin/static/media/bg.a568162c.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/hanxin/static/media/logo.ee7cd8ed.svg"
+  }
+]);
